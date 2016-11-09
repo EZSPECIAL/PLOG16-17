f9b8deef5a68ce837035f9578472e9b399595bf2
EZSPECIAL <ftlopes93@gmail.com>
2016-11-09 22:37:44 +0000

    Finished board display function

--- facts.pl
+++ facts.pl
@@ -9,7 +9,7 @@ position(x, 0, 0).
 
 position(y, 0, 7).
 
-init([['S',' ',' ',' ',' ',' ',' ','c'],
+stage1([['S',' ',' ',' ',' ',' ',' ','c'],
      [' ',' ',' ',' ',' ',' ',' ',' '],
      [' ',' ',' ',' ',' ',' ',' ',' '],
      [' ',' ',' ','N','N',' ',' ',' '],
@@ -18,7 +18,7 @@ init([['S',' ',' ',' ',' ',' ',' ','c'],
      [' ',' ',' ',' ',' ',' ',' ',' '],
      ['C',' ',' ',' ',' ',' ',' ','s']]).
 
-intermediate([['S','156','237','137','346','167','134','c'],
+stage2([['S','156','237','137','346','167','134','c'],
      ['457','246','128','245','456','178','458','258'],
      ['135','138','278','678','168','136','347','267'],
      ['468','234','345','N','N','146','157','236'],
@@ -29,14 +29,6 @@ intermediate([['S','156','237','137','346','167','134','c'],
 
 test([['123','124'],['125','123']]).
 
-final([['S','156','237','137','346','167','134','c'],
-     ['457','246','128','245','456',['178','Y'],'458','258'],
-     ['135','138','278','678','168','136','347','267'],
-     ['468','234','345','N','N','146','157','236'],
-     ['467','124','578','N','N','368','247','367'],
-     ['158','357','147','568','148','125','358','178'],
-     ['478','145','256','235','348','127','123','356'],
-     ['C','248','268','126','567','257','238',['s','X']]]).
 
 buildCell('123', Player, [' ', '|', '/', '|', ' ', Var, '-', '|', ' ', ' ', ' ', '|']) :-
 	(Player = 0, Var = ' ', !; Var = Player).
@@ -46,6 +38,181 @@ buildCell('124', Player, [' ', '|', '/', '|', ' ', Var, ' ', '|', ' ', ' ', '\\'
 	
 buildCell('125', Player, [' ', '|', '/', '|', ' ', Var, ' ', '|', ' ', '|', ' ', '|']) :-
 	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('126', Player, [' ', '|', '/', '|', ' ', Var, ' ', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('127', Player, [' ', '|', '/', '|', '-', Var, ' ', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+
+buildCell('128', Player, ['\\', '|', '/', '|', ' ', Var, ' ', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('134', Player, [' ', '|', ' ', '|', ' ', Var, '-', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('135', Player, [' ', '|', ' ', '|', ' ', Var, '-', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+
+buildCell('136', Player, [' ', '|', ' ', '|', ' ', Var, '-', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+
+buildCell('137', Player, [' ', '|', ' ', '|', '-', Var, '-', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('138', Player, ['\\', '|', ' ', '|', ' ', Var, '-', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('145', Player, [' ', '|', ' ', '|', ' ', Var, ' ', '|', ' ', '|', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('146', Player, [' ', '|', ' ', '|', ' ', Var, ' ', '|', '/', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('147', Player, [' ', '|', ' ', '|', '-', Var, ' ', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('148', Player, ['\\', '|', ' ', '|', ' ', Var, ' ', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('156', Player, [' ', '|', ' ', '|', ' ', Var, ' ', '|', '/', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('157', Player, [' ', '|', ' ', '|', '-', Var, ' ', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('158', Player, ['\\', '|', ' ', '|', ' ', Var, ' ', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('167', Player, [' ', '|', ' ', '|', '-', Var, ' ', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('168', Player, ['\\', '|', ' ', '|', ' ', Var, ' ', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('178', Player, ['\\', '|', ' ', '|', '-', Var, ' ', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('234', Player, [' ', ' ', '/', '|', ' ', Var, '-', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('235', Player, [' ', ' ', '/', '|', ' ', Var, '-', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('236', Player, [' ', ' ', '/', '|', ' ', Var, '-', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('237', Player, [' ', ' ', '/', '|', '-', Var, '-', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('238', Player, ['\\', ' ', '/', '|', ' ', Var, '-', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('245', Player, [' ', ' ', '/', '|', ' ', Var, ' ', '|', ' ', '|', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+
+buildCell('246', Player, [' ', ' ', '/', '|', ' ', Var, ' ', '|', '/', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('247', Player, [' ', ' ', '/', '|', '-', Var, ' ', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('248', Player, ['\\', ' ', '/', '|', ' ', Var, ' ', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('256', Player, [' ', ' ', '/', '|', ' ', Var, ' ', '|', '/', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('257', Player, [' ', ' ', '/', '|', '-', Var, ' ', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('258', Player, ['\\', ' ', '/', '|', ' ', Var, ' ', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('267', Player, [' ', ' ', '/', '|', '-', Var, ' ', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('268', Player, ['\\', ' ', '/', '|', ' ', Var, ' ', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('278', Player, ['\\', ' ', '/', '|', '-', Var, ' ', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('345', Player, [' ', ' ', ' ', '|', ' ', Var, '-', '|', ' ', '|', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('346', Player, [' ', ' ', ' ', '|', ' ', Var, '-', '|', '/', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('347', Player, [' ', ' ', ' ', '|', '-', Var, '-', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('348', Player, ['\\', ' ', ' ', '|', ' ', Var, '-', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('356', Player, [' ', ' ', ' ', '|', ' ', Var, '-', '|', '/', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('357', Player, [' ', ' ', ' ', '|', '-', Var, '-', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+
+buildCell('358', Player, ['\\', ' ', ' ', '|', ' ', Var, '-', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('367', Player, [' ', ' ', ' ', '|', '-', Var, '-', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('368', Player, ['\\', ' ', ' ', '|', ' ', Var, '-', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('378', Player, ['\\', ' ', ' ', '|', '-', Var, '-', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('456', Player, [' ', ' ', ' ', '|', ' ', Var, ' ', '|', '/', '|', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('457', Player, [' ', ' ', ' ', '|', '-', Var, ' ', '|', ' ', '|', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('458', Player, ['\\', ' ', ' ', '|', ' ', Var, ' ', '|', ' ', '|', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('467', Player, [' ', ' ', ' ', '|', '-', Var, ' ', '|', '/', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('468', Player, ['\\', ' ', ' ', '|', ' ', Var, ' ', '|', '/', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('478', Player, ['\\', ' ', ' ', '|', '-', Var, ' ', '|', ' ', ' ', '\\', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('567', Player, [' ', ' ', ' ', '|', '-', Var, ' ', '|', '/', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('568', Player, ['\\', ' ', ' ', '|', ' ', Var, ' ', '|', '/', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('578', Player, ['\\', ' ', ' ', '|', '-', Var, ' ', '|', ' ', '|', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('678', Player, ['\\', ' ', ' ', '|', '-', Var, ' ', '|', '/', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('S', Player, ['S', ' ', ' ', '|', ' ', Var, ' ', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('s', Player, [' ', ' ', ' ', '|', ' ', Var, ' ', '|', ' ', ' ', 's', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('C', Player, [' ', ' ', ' ', '|', ' ', Var, ' ', '|', 'C', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+
+buildCell('c', Player, [' ', ' ', 'c', '|', ' ', Var, ' ', '|', ' ', ' ', ' ', '|']) :-
+	(Player = 0, Var = ' ', !; Var = Player).
+	
+buildCell('N', Player, [' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|']).
+
+buildCell(' ', Player, [' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|']).
 
 playGame(Board) :-
         viewBoard(Board).
@@ -79,7 +246,7 @@ printTest :-
         printDotted.
 
 printDotted :-
-        write(------------------------).
+        write(--------------------------------).
 
 printLine([]).
 
